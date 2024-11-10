dddcc63ffc4451e77c6ddf5e50b47078f0dbb0bc
Samara Barcelos de Lima <130487962+SamySLB@users.noreply.github.com>
2024-11-09 21:57:27 -0300

    add_Profile_information

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -6,7 +6,7 @@ import Footer from "@/components/footer/footer"
 export default function Profile(){
     return(
     <div>
-            {/* <Header/> */}
+            <Header/>
             <Perfil/>
             <MyHistory/>
             <Experiences/>
